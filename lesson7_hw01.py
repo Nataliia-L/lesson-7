7618fd9eea1c4abaf1ca57e093fab76bc4fb7c61
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-23 16:28:25 +0200

    Delete lesson7_hw01.py

--- lesson7_hw01.py
+++
@@ -1,6 +0,0 @@
-st = "harry potter; 30; 127.45 galeon; auror"
-st1 = st.split(";")
-st2 = st1[0].split()
-st3 = st1[2].split()
-d = {"name":{"first_name":st2[0].title(), "last_name": st2[1].title()}, "age": st1[1], "profession": st1[3], "salary": {"amount": st3[0], "currency": st3[1]}}
-print (d)
