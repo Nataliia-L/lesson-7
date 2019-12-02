66a4fb0df23c25da8c3936aec4fd726e9298ee01
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-12-02 09:20:08 +0200

    Update lesson7_hw02.py

--- lesson7_hw02.py
+++ lesson7_hw02.py
@@ -1,3 +1,6 @@
-val = "abcdefghijklmnopqrstuvwxyz"
-d = {'a': val[3], 'b': val[4], 'c':val[5], 'd':val[6], 'e':val[7], 'f':val[8], 'g':val[9], 'h':val[10], 'i':val[11], 'j':val[12], 'k':val[13], 'l':val[14], 'm':val[15], 'n':val[16], 'o':val[17], 'p':val[18], 'q':val[19], 'r':val[20], 's':val[21], 't':val[22], 'u':val[23], 'v':val[24], 'w':val[25], 'x':val[0], 'y':val[1], 'z':val[2]}
+import string
+step = 3
+keys = list(string.ascii_lowercase) 
+values = list(string.ascii_lowercase[step:] + string.ascii_lowercase[:step]) 
+d = dict (zip(keys, values))
 print (d)
